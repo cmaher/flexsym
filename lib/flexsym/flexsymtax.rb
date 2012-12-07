7a89b03fcaacf409131d1f5dfbbdfc5a983e9e0b
cmaher <maher.cs@gmail.com>
2012-12-07 17:35:23 -0500

    Adding code hashes to syntax for reference outside. Referencing said hashing in unflex.  getting ready for prettyish parsing

--- lib/flexsym/flexsymtax.rb
+++ lib/flexsym/flexsymtax.rb
@@ -1,31 +1,41 @@
 module Flexsym
     module Flexsymtax
-        def program main_ref, states
+        CODES = {
+            succ:   0x0,
+            pred:   0x1,
+            left:   0x2,
+            right:  0x3,
+            out:    0x4,
+            void:   0x5,
+            quote:  0xF
+        }
+
+        def self.program main_ref, states
             [:program, main_ref, states]
         end
 
-        def state ref, default, branches
+        def self.state ref, default, branches
             [:state, ref, default, branches]
         end
 
-        def label text
+        def self.label text
             [:label, text]
         end
 
-        def branch condition, block
+        def self.branch condition, block
             [:branch, condition, block]
         end
 
-        def byte value
+        def self.byte value
             [:byte, value]
         end
 
-        def block cmd1, cmd2, cmd3, cmd4
+        def self.block cmd1, cmd2, cmd3, cmd4
             [:block, cmd1, cmd2, cmd3, cmd4]
         end
 
-        def op value
-            [:op, value]
+        def self.op opcode
+            [:op, opcode]
         end
     end
 end
--- lib/flexsym/unflexsymast.rb
+++ lib/flexsym/unflexsymast.rb
@@ -1,4 +1,29 @@
+require 'rparsec'
+require 'flexsymtax'
+
 module Flexsym
     class Unflexsymast
+        include Parsers
+
+        C_SUCC  = :'+'
+        C_PRED  = :'-'
+        C_LEFT  = :'<'
+        C_RIGHT = :'>'
+        C_OUT   = :'^'
+        C_VOID  = :'_'
+        C_QUOTE = :';'
+        
+        CODES = {
+            C_SUCC  => Flexsymtax.CODES[:succ],
+            C_PRED  => Flexsymtax.CODES[:pred], 
+            C_LEFT  => Flexsymtax.CODES[:left],
+            C_RIGHT => Flexsymtax.CODES[:right],
+            C_OUT   => Flexsymtax.CODES[:out],
+            C_VOID  => Flexsymtax.CODES[:void],
+            C_QUOTE => Flexsymtax.CODES[:quote]
+        }
+
+        def parser
+        end
     end
 end
