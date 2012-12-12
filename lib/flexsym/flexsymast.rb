01b24b99fa77da966d78697cceb3c09583f53b5b
cmaher <maher.cs@gmail.com>
2012-12-11 19:44:06 -0500

    flexsym library 'completed', needs to be tested, and called from script

--- lib/flexsym/flexsymast.rb
+++ lib/flexsym/flexsymast.rb
@@ -1,16 +1,41 @@
-require_relative 'flexsymtax'
+require_relative 'unflexsymast'
 
 module Flexsym
     class Flexsymast
+
+        # Nibble code for QUOTE
+        Q_N = 0xF
         CODES = {
-            0x0 => Flexsymtax::O_SUCC,
-            0x1 => Flexsymtax::O_PRED,
-            0x2 => Flexsymtax::O_LEFT,
-            0x3 => Flexsymtax::O_RIGHT,
-            0x4 => Flexsymtax::O_OUTA,
-            0x5 => Flexsymtax::O_OUTD,
-            0x6 => Flexsymtax::O_VOID,
-            0xF => :quote
+            0x0 => Unflexsymast::C_SUCC,
+            0x1 => Unflexsymast::C_PRED,
+            0x2 => Unflexsymast::C_LEFT,
+            0x3 => Unflexsymast::C_RIGHT,
+            0x4 => Unflexsymast::C_OUTA,
+            0x5 => Unflexsymast::C_OUTD,
+            0x6 => Unflexsymast::C_VOID,
+            Q_N => Unflexsymast::C_QUOTE
         }
+
+        def parse
+            quoting = false
+            @source_file.each_byte do |b|
+                # High-order, Low-order nibbles
+                nibbles = [b >> 4, b & 0x0F]
+                nibbles.each do |n|  
+                    quoting = !quoting if n == Q_N
+                    if quoting
+                        @source << n.chr
+                    elsif CODES[n] 
+                        @source << CODES[n]
+                    end
+                end
+            end
+            Unflexsymast.new(@source).parse
+        end
+
+        def initialize(source_file)
+            @source_file = source_file
+            @source = []
+        end
     end
 end
--- lib/flexsym/unflexsymast.rb
+++ lib/flexsym/unflexsymast.rb
@@ -5,77 +5,82 @@ module Flexsym
     class Unflexsymast
         include RParsec::Parsers
 
-        C_SUCC  = '+'
-        C_PRED  = '-'
-        C_LEFT  = '<'
-        C_RIGHT = '>'
-        C_OUTA  = '^'
-        C_OUTD  = '.' 
-        C_VOID  = '_'
-        C_QUOTE = ';'
+        C_SUCC  = ?+
+        C_PRED  = ?-
+        C_LEFT  = ?<
+        C_RIGHT = ?>
+        C_OUTA  = ?^
+        C_OUTD  = ?. 
+        C_VOID  = ?_
+        C_QUOTE = ?;
        
-        OPS = [C_SUCC, C_PRED, C_LEFT, C_RIGHT, C_OUT, C_VOID]
+        OPS = [C_SUCC, C_PRED, C_LEFT, C_RIGHT, C_OUTA, C_OUTD, C_VOID]
 
         OPCODES = {
             C_SUCC  => Flexsymtax::O_SUCC,
             C_PRED  => Flexsymtax::O_PRED, 
-            C_LEFT  => Flexstymtax::O_LEFT,
+            C_LEFT  => Flexsymtax::O_LEFT,
             C_RIGHT => Flexsymtax::O_RIGHT,
             C_OUTA  => Flexsymtax::O_OUTA,
             C_OUTD  => Flexsymtax::O_OUTD,
             C_VOID  => Flexsymtax::O_VOID,
         }
         
-        def parser_ignore
+        def self.parser_ignore
            not_among(*OPS, C_QUOTE, *%w{0 1 2 3 4 5 6 7 8 9}).many
         end
 
-        def parser_label
+        def self.parser_label
             quote = char C_QUOTE
             text = not_among(C_QUOTE).many
-            parser_ignore >> sequence(quote, text, quote) do |_, chars, _|
+            self.parser_ignore >> sequence(quote, text, quote) do |_, chars, _|
                 Flexsymtax.label(chars.join)
             end
         end
 
-        def parser_num
-            parser_ignore >> integer.map{|num| Flexsymtax.num(num)}
+        def self.parser_num
+            self.parser_ignore >> integer.map{|num| Flexsymtax.num(num)}
         end
 
-        def parser_op
-           parser_ignore >> among(*OPS).map{|op| Flexsymtax.op(OPCODES[op])}
+        def self.parser_op
+            self.parser_ignore >> among(*OPS).map{|op| Flexsymtax.op(OPCODES[op])}
         end
 
-        def parser_block
-            cmd = parser_op | parser_label
-            parser_ignore >> cmd.repeat(4, 4).map do |c1, c2, c3, c4|
+        def self.parser_block
+            cmd = self.parser_op | self.parser_label
+            self.parser_ignore >> cmd.repeat(4, 4).map do |c1, c2, c3, c4|
                 Flexsymtax.block(c1, c2, c3, c4)
             end
         end
 
-        def parser_branch
-            parser_ignore >> sequence(parser_num, parser_block) do |condition, block|
+        def self.parser_branch
+            self.parser_ignore >> sequence(self.parser_num, self.parser_block) do |condition, block|
                 Flexsymtax.branch(condition, block)
             end
         end
 
-        def parser_state
-            branches = parser_branch.many
-            parser_ignore >> sequence(parser_label, parser_block, branches) do |ref, default, branches|
+        def self.parser_state
+            branches = self.parser_branch.many
+            self.parser_ignore >> 
+                sequence(self.parser_label, self.parser_block, branches) do |ref, default, branches|
                 Flexsymtax.state(ref, default, branches)
             end
         end
 
-        def parser_program
-            states = parser_state.many(1)
-            parser_ignore >> sequence(parser_label, states) do |main_ref, states|
+        def self.parser_program
+            states = self.parser_state.many(1)
+            self.parser_ignore >> sequence(self.parser_label, states) do |main_ref, states|
                 Flexsymtax.program(main_ref, states)
             end
         end
 
-        def parse(source)
-            parser_program.parse(source)
+        def parse
+            self.parser_program.parse(@source)
         end
 
+        # Read the string and parse it 
+        def initialize(source_string)
+            @source = source_string
+        end
     end
 end
