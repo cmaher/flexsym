0eb75ba84bb81a7cf1ce8bdc07b4c6f2087f2276
cmaher <maher.cs@gmail.com>
2012-12-15 19:35:20 -0500

    Adding programs to unit tests

---
+++ tests/tc_programs.rb
@@ -0,0 +1,21 @@
+require 'test/unit'
+require 'flexsym'
+
+class TestExamples < Test::Unit::TestCase
+    include Flexsym
+
+    def test_hello
+        output = `ruby bin/flexsym examples/hello.flexsym`
+        assert_equal("Hello, World!\n", output)
+    end
+
+    def test_min
+        output = `ruby bin/flexsym examples/min.flexsym`
+        assert_equal("", output)
+    end
+
+    def test_nd4
+        output = `ruby bin/flexsym examples/nd4.flexsym`
+        assert_equal("4444\n", output)
+    end
+end
