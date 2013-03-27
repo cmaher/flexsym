1ee1eb37f064f8d167b5fa1e06da113777796aca
Christian Maher <maher.cs@gmail.com>
2013-03-27 09:54:27 -0300

    refactoring

--- lib/flexsym/flexsymachine.rb
+++ lib/flexsym/flexsymachine.rb
@@ -8,55 +8,60 @@ module Flexsym
     end
 
     # Performs one iteration on a machine
+    # Halts on no new states
     # Returns:
     # * A list of new states for nondeterministic branching
     # * The tape, for duplicating and passing to new machines
     def step
-      # Init the list of new machines
-      next_states = []
+      unless @halt
+        next_states = exec_blocks(get_blocks)
+        
+        if next_states.empty?
+          @halt = true
+        else
+          @cur_state = next_states.pop
+        end
+        
+        return next_states, @tape
+      end
+    end
 
-      # Get the branches corresponding to the current tape value
-      # Or choose the default
-      branches = @cur_state.branches[@tape.val]
-      if branches
-        blocks = branches.map{ |branch| branch.block } 
+    private
+    
+    # Get the default blocks or blocks for current tape value
+    def get_blocks
+      if (branches = @cur_state.branches[@tape.val]
+        branches.map { |branch| branch.block } 
       else
-        blocks = [@cur_state.default]
+        [@cur_state.default]
       end
-
-      # execute each block
-      blocks.each do |block|
-        cmds = block.commands
-        exec_tape(cmds)
-        exec_out(cmds)
-        exec_head(cmds)
-        exec_trans(cmds, next_states)
-      end
-
-      if next_states.empty?
-        # If this block doesn't transition, it's the end of the line
-        @halt = true
-      else
-        # Else, give one of the next states to this machine
-        @cur_state = next_states.pop
-      end
-
-      # Return the remaining states (and the tape for duping)
-      return next_states, @tape
+    end
+    
+    # Execute all of the given blocks and return the new states
+    def exec_blocks(blocks)
+      blocks.map { |block| exec_commands(block.commands) }
+    end
+    
+    # Execute all of the given commands and return the new state
+    def exec_commands(cmds)
+      exec_tape(cmds)
+      exec_out(cmds)
+      exec_head(cmds)
+      exec_trans(cmds)
     end
 
     # Perform tape cell operation (:succ/:pred)
-    def exec_tape(block)
-      @tape.cell(block[:tape]) if block[:tape]
+    def exec_tape(cmds)
+      @tape.cell(cmds[:tape]) if cmds[:tape]
     end
 
     # Perform tape head operation (:left/:right)
-    def exec_head(block)
-      @tape.move(block[:head]) if block[:head]
+    def exec_head(cmds)
+      @tape.move(cmds[:head]) if cmds[:head]
     end
 
     # Perform output (:outa/:outd)
-    def exec_out(block)
+    def exec_out(cmds)
       val = @tape.val
       case block[:out]
       when Flexsymtax::O_OUTA then print [val].pack('U')
@@ -65,13 +70,9 @@ module Flexsym
     end
 
     # Return the transition state
-    def exec_trans(block, next_states)
-      if block[:trans]
-        if (s = @states[block[:trans]])
-          next_states << s
-        else
-          fail "Unrecognized state: ;#{block[:trans]};"
-        end
+    def exec_trans(cmds)
+      if (trans = cmds[:trans] && (next_state = @states[trans])
+          next_state
       end
     end
 
